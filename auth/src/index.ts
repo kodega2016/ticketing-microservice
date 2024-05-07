af414200f972238b2472b283db7d9a09ea2e7d9d
kodega2016 <khadgalovecoding2016@gmail.com>
2024-05-07 19:18:27 +0545

    setup: updated skaffold yaml

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,5 @@
-import express from "express";
 import { json } from "body-parser";
+import express from "express";
 
 const app = express();
 
@@ -7,5 +7,5 @@ const app = express();
 app.use(json());
 
 app.listen(3000, () => {
-  console.log("Listening on port 3000");
+  console.log("(auth) Listening on port 3000");
 });
