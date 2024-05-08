6ef6e410e14fc8856720bc43f2ed96f863fe7a7f
kodega2016 <khadgalovecoding2016@gmail.com>
2024-05-08 16:18:02 +0545

    setup: added validators middleware and validate sign in

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,11 +1,18 @@
-import express from "express";
-const router = express.Router();
+import { Request, Response, Router } from "express";
+import { signinValidator } from "../validators/signin";
+import { validator } from "../middlewares/validator";
+const router = Router();
 
-router.post("/api/users/signin", (req, res) => {
-  res.status(200).json({
-    message: "signed in successfully",
-    data: null,
-  });
-});
+router.post(
+  "/api/users/signin",
+  signinValidator,
+  validator,
+  (req: Request, res: Response) => {
+    res.status(200).json({
+      message: "signed in successfully",
+      data: null,
+    });
+  },
+);
 
 export { router as signinRouter };
