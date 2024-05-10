6fec8ec813fc550fad9eee9d40b92f1f681e8cb5
kodega2016 <khadgalovecoding2016@gmail.com>
2024-05-10 19:28:23 +0545

    feat: added requireAuth and not authorized error

--- auth/src/routes/currentuser.ts
+++ auth/src/routes/currentuser.ts
@@ -1,13 +1,15 @@
 import express, { Request, Response } from "express";
 import { currentUser } from "../middlewares/current-user";
+import { requireAuth } from "../middlewares/require-auth";
 const router = express.Router();
 
 router.get(
   "/api/users/currentuser",
   currentUser,
+  requireAuth,
   async (req: Request, res: Response) => {
     res.status(200).json({
-      message: "currentuser is fetched successfully",
+      message: "current user is fetched successfully",
       data: req.currentUser || null,
     });
   }
