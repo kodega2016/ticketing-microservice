9c68e58f56e6e0f1cb669250acff67952ad62e5c
kodega2016 <khadgalovecoding2016@gmail.com>
2024-05-10 20:43:10 +0545

    feat: added test cases for signup

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -10,3 +10,49 @@ it("returns a 201 on successful signup", async () => {
     })
     .expect(201);
 });
+
+it("returns a 400 with an invalid email", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test.com",
+      password: "password",
+    })
+    .expect(400);
+});
+
+it("returns a 400 with an invalid password", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: null,
+    })
+    .expect(400);
+});
+
+it("returns a 400 with an invalid email and password", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: null,
+      password: null,
+    })
+    .expect(400);
+
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: null,
+    })
+    .expect(400);
+
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: null,
+      password: "password",
+    })
+    .expect(400);
+});
