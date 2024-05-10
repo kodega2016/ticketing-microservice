584ae8c86e7db192e19a33c0f737deaadeb54da4
kodega2016 <khadgalovecoding2016@gmail.com>
2024-05-10 07:58:38 +0545

    feat: added current user middleware

--- auth/src/routes/currentuser.ts
+++ auth/src/routes/currentuser.ts
@@ -1,26 +1,16 @@
 import express, { Request, Response } from "express";
-import { BadRequestError } from "../errors/bad-request-error";
-import { Jwt } from "../services/jwt";
-import { User } from "../models/user";
+import { currentUser } from "../middlewares/current-user";
 const router = express.Router();
 
-router.get("/api/users/currentuser", async (req: Request, res: Response) => {
-  if (!req.session?.jwt) {
-    return res.status(200).json({
-      message: "currentuser is fetched successfully",
-      data: null,
-    });
-  }
-  try {
-    const payload = Jwt.verify(req.session.jwt);
-    const user = await User.findById(payload.id);
+router.get(
+  "/api/users/currentuser",
+  currentUser,
+  async (req: Request, res: Response) => {
     res.status(200).json({
       message: "currentuser is fetched successfully",
-      data: user,
+      data: req.currentUser || null,
     });
-  } catch (err) {
-    throw new BadRequestError("invalid token");
   }
-});
+);
 
 export { router as currentUserRouter };
