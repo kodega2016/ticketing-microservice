9a71c51dd0d36edd5ff884771d8bde1c75317133
kodega2016 <khadgalovecoding2016@gmail.com>
2024-05-11 23:31:41 +0545

    added sign put

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,33 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import buildClient from "../api/build-client";
+import Header from "../components/header";
 
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps}></Component>;
+const AppComponent = ({ Component, pageProps, data }) => {
+  return (
+    <div className="container">
+      <Header currentUser={data} />
+      <Component {...pageProps} />
+    </div>
+  );
 };
+
+AppComponent.getInitialProps = async (appContext) => {
+  try {
+    const client = buildClient(appContext.ctx);
+    const { data } = await client.get("/api/users/currentuser");
+    let pageProps = {};
+
+    if (appContext.Component.getInitialProps) {
+      pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+    }
+    return {
+      pageProps,
+      ...data,
+    };
+  } catch (error) {
+    console.log(error.message);
+    return {};
+  }
+};
+
+export default AppComponent;
--- client/pages/index.js
+++ client/pages/index.js
@@ -5,20 +5,16 @@ const Landing = ({ data }) => {
     <h1>You are signed in</h1>
   ) : (
     <div>
-      <h1>You are not signed in</h1>;<a href="/auth/signin">Sign in</a>
+      <h1>You are not signed in</h1>
+      <a href="/auth/signin">Sign in</a>
     </div>
   );
 };
 
 Landing.getInitialProps = async (context) => {
-  try {
-    const client = buildClient(context);
-    const { data } = await client.get("/api/users/currentuser");
-    return data;
-  } catch (e) {
-    console.log(e.message);
-    return {};
-  }
+  const client = buildClient(context);
+  const { data } = await client.get("/api/users/currentuser");
+  return data;
 };
 
 export default Landing;
