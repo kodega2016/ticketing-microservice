86b08656cfedfc3ade94ecb25fbf74625495f9f9
kodega2016 <khadgalovecoding2016@gmail.com>
2024-05-11 07:23:22 +0545

    added use request hook for api request

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,38 +1,37 @@
 import { useState } from "react";
 import axios from "axios";
+import useRequest from "../../hooks/use-request";
 
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [errors, setErrors] = useState([]);
+
+  const { doRequest, errors } = useRequest({
+    url: "/api/users/signup",
+    method: "post",
+    body: { email, password },
+  });
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("email:", email, "password:", password);
-    try {
-      await axios.post("/api/users/signup", {
-        email,
-        password,
-      });
-      setErrors([]);
-    } catch (error) {
-      setErrors(error.response.data.errors);
-    }
+    const response = await doRequest();
+    console.log("response:", response);
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <div className="container mx-auto">
         <h1>Signup page</h1>
-        {errors && errors.length > 0 && (
-          <div className="alert alert-danger">
-            <ul className="my-0">
-              {errors.map((err) => (
-                <li key={err.message}>{err.message}</li>
-              ))}
-            </ul>
-          </div>
-        )}
+        {/* {errors && errors.length > 0 && ( */}
+        {/*   <div className="alert alert-danger"> */}
+        {/*     <ul className="my-0"> */}
+        {/*       {errors.map((err) => ( */}
+        {/*         <li key={err.message}>{err.message}</li> */}
+        {/*       ))} */}
+        {/*     </ul> */}
+        {/*   </div> */}
+        {/* )} */}
+        {errors && errors}
         <div className="form-group">
           <label>Email Address</label>
           <input
