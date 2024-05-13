fce17b4a2b6642d9f8557aa5bd84e9a869571c24
kodega2016 <khadgalovecoding2016@gmail.com>
2024-05-14 04:11:50 +0545

    added mongo db deployment for tickets deployment

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,9 +1,25 @@
 import { app } from "./app";
+import mongoose from "mongoose";
 
 const start = async () => {
-  app.listen(3000, () => {
-    console.log("(tickets) Listening on port 3000");
-  });
+  if (process.env.MONGO_DB_URI === undefined) {
+    throw new Error("MONGO_DB_URI must be defined");
+  }
+
+  try {
+    const response = await mongoose.connect(
+      `mongodb://${process.env.MONGO_DB_URI}:27017/tickets`
+    );
+    console.log(
+      `Database connection made to:${response.connection.db.databaseName} `
+    );
+    app.listen(3000, () => {
+      console.log("(tickets) Listening on port 3000");
+    });
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
 };
 
 start();
