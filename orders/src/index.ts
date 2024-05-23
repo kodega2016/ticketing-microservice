8e721bf817b2aae8d4a65486abf99c021e3da355
kodega2016 <khadgalovecoding2016@gmail.com>
2024-05-23 23:13:33 +0545

    added orders mongo db connection

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -1,12 +1,28 @@
 import { app } from "./app";
+import mongoose from "mongoose";
 
 const start = async () => {
   if (process.env.JWT_KEY === undefined) {
     throw new Error("JWT_KEY must be defined");
   }
-  app.listen(3000, () => {
-    console.log("orders service listening on port 3000");
-  });
+  if (process.env.MONGO_DB_URI === undefined) {
+    throw new Error("MONGO_DB_URI must be defined");
+  }
+
+  try {
+    const response = await mongoose.connect(
+      `mongodb://${process.env.MONGO_DB_URI}:27017/orders`,
+    );
+    console.log(
+      `Database connection made to:${response.connection.db.databaseName} `,
+    );
+    app.listen(3000, () => {
+      console.log("orders service listening on port 3000");
+    });
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
 };
 
 start();
