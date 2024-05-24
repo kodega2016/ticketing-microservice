8864e45e29da4af5cd0b48f9adaf0bdbe2256c66
kodega2016 <khadgalovecoding2016@gmail.com>
2024-05-24 06:38:06 +0545

    added nats client to order service

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -1,5 +1,6 @@
 import { app } from "./app";
 import mongoose from "mongoose";
+import { natsWrapper } from "./nats-wrapper";
 
 const start = async () => {
   if (process.env.JWT_KEY === undefined) {
@@ -9,7 +10,34 @@ const start = async () => {
     throw new Error("MONGO_DB_URI must be defined");
   }
 
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error("NATS_CLUSTER_ID must be defined");
+  }
+
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("NATS_CLIENT_ID must be defined");
+  }
+
+  if (!process.env.NATS_URL) {
+    throw new Error("NATS_URL must be defined");
+  }
+
   try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL,
+    );
+    console.log("Connected to NATS");
+
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed");
+      process.exit();
+    });
+
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+
     const response = await mongoose.connect(
       `mongodb://${process.env.MONGO_DB_URI}:27017/orders`,
     );
