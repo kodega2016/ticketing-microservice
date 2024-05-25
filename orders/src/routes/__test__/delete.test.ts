1bd6013facb51e84d7535fecd2f89fd0976d271d
kodega2016 <khadgalovecoding2016@gmail.com>
2024-05-25 17:13:06 +0545

    added remaining test cases for order service

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -2,6 +2,7 @@ import request from "supertest";
 import { app } from "../../app";
 import mongoose from "mongoose";
 import { Ticket } from "../../models/ticket";
+import { natsWrapper } from "../../nats-wrapper";
 
 it("returns a 404 if the provided id does not exist", async () => {
   const id = new mongoose.Types.ObjectId().toHexString();
@@ -58,4 +59,24 @@ it("marks an order as cancelled", async () => {
     .set("Cookie", user)
     .send()
     .expect(204);
+});
+
+it("emits an order cancelled event", async () => {
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
+  await ticket.save();
+  const user = global.signin();
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+  await request(app)
+    .delete(`/api/orders/${order.data.id}`)
+    .set("Cookie", user)
+    .send()
+    .expect(204);
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
