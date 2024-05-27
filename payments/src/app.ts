1ccf5e541f25edb5a8b0028ffe9691c4837e403d
kodega2016 <khadgalovecoding2016@gmail.com>
2024-05-27 23:11:27 +0545

    added payments service

---
+++ payments/src/app.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import cookieSession from "cookie-session";
+import "express-async-errors";
+import { NotFoundError, currentUser, errorHandler } from "@kodeapps/common";
+
+const app = express();
+// setup body parser
+app.use(express.json());
+// setup cookie parser
+app.set("trust proxy", true);
+app.use(
+  cookieSession({
+    httpOnly: false,
+    signed: false,
+    secure: process.env.NODE_ENV !== "test",
+  })
+);
+
+// setup current user middleware
+app.use(currentUser);
+
+// not found route handler
+app.get("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
+// setup error handler
+app.use(errorHandler);
+
+export { app };
---
+++ payments/src/index.ts
@@ -0,0 +1,57 @@
+import { app } from "./app";
+import { natsWrapper } from "./nats-wrapper";
+import mongoose from "mongoose";
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be defined");
+  }
+
+  if (process.env.MONGO_DB_URI === undefined) {
+    throw new Error("MONGO_DB_URI must be defined");
+  }
+  if (process.env.NATS_CLUSTER_ID === undefined) {
+    throw new Error("NATS_CLUSTER_ID must be defined");
+  }
+
+  if (process.env.NATS_CLIENT_ID === undefined) {
+    throw new Error("NATS_CLIENT_ID must be defined");
+  }
+
+  if (process.env.NATS_URL === undefined) {
+    throw new Error("NATS_URL must be defined");
+  }
+
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+    console.log("connected to NATS");
+
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed");
+      process.exit();
+    });
+
+    // listen for interupt signals
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+
+    const response = await mongoose.connect(
+      `mongodb://${process.env.MONGO_DB_URI}:27017/payments`
+    );
+    console.log(
+      `Database connection made to:${response.connection.db.databaseName} `
+    );
+    app.listen(3000, () => {
+      console.log("(payments) Listening on port 3000");
+    });
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+start();
