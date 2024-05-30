c134a4237a4c6d803c1dca1893185256a864b016
kodega2016 <khadgalovecoding2016@gmail.com>
2024-05-30 23:00:53 +0545

    feat: added new order integration

--- client/pages/tickets/[ticketId].js
+++ client/pages/tickets/[ticketId].js
@@ -1,9 +1,25 @@
+import useRequest from "../../hooks/use-request";
+
 const Ticketshow = ({ ticket }) => {
+  const { doRequest, errors } = useRequest({
+    url: "/api/orders",
+    method: "post",
+    body: {
+      ticketId: ticket.id,
+    },
+    onSuccess: (order) => {
+      console.log(order);
+    },
+  });
+
   return (
     <div>
       <h2>{ticket.title}</h2>
       <h4>Price: {ticket.price}</h4>
-      <button className="btn btn-primary">Purchase</button>
+      <button className="btn btn-primary" onClick={doRequest}>
+        Purchase
+      </button>
+      {errors && errors}
     </div>
   );
 };
